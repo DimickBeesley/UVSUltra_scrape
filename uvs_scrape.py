1794c3f8448fc500113f5e5534c339a9097ea3bc
DimickBeesley <dbeesley8@gmail.com>
2023-06-29 02:34:11 -0500

    Wrote Script to check which IDs are in use

--- uvs_scrape.py
+++ uvs_scrape.py
@@ -1,8 +1,27 @@
 import pandas as pd
 import re
 
+import time
+import csv
+
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+
+
+
+
+import socket
+from urllib3.connection import HTTPConnection
+
+HTTPConnection.default_socket_options = (
+    HTTPConnection.default_socket_options + [
+        (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+        (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
+        (socket.SOL_TCP, socket.TCP_KEEPCNT, 6)
+    ]
+)
+
+
 
 
 session = HTMLSession()
@@ -40,23 +59,41 @@ def parse_soup(response):
 
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    card_name = soup.select("div.card_title h1")    
+    card_image = soup.select("div.card_image")    
     #TODO Make sure the return value when we do this is handled in a good way
-    if (len(card_name) == 0): return
+    print(len(card_image))
+    #if (len(card_image) == 0): return
 
+    """
     dictionary = {
         "card_name": card_name[0].text.strip(),
         "random_stuff_1": soup.select("div.card_division.cd1")[0].text.strip(), # I actually don't know what this will be hitting
         "card_text": soup.select("#text")[0].text.strip(),
         "int_values": soup.select("div.card_division.cd3")[0].text.strip()
     }
+    """
     
-    return dictionary
+    return #dictionary
 
-response = request_card_w_id("1", session)
-dictionary = parse_soup(response)
+used_ids = []
 
+for i in range(0,13000):
+    response = request_card_w_id("1", session).text
+    temp_soup = BeautifulSoup(response, 'html.parser')
+    temp = temp_soup.select("div.card_image")
+    if len(temp) == 0:
+        used_ids.append(0)
+    if len(temp) == 1:
+        used_ids.append(1)
+    if len(temp) > 1:
+        used_ids.append(2)
+
+print(used_ids)
+
+
+"""
 print(dictionary["card_name"])
 print(dictionary["random_stuff_1"])
 print(dictionary["card_text"])
 print(dictionary["int_values"])
+"""
